b23da5ab79ef486d138f54cb1f0bd30dd989d990
VaatuDev <juanmena1605@gmail.com>
2024-06-09 18:18:05 -0500

    fix: optimize ai-page children render

--- src/app/ai/prompt/page.tsx
+++ src/app/ai/prompt/page.tsx
@@ -3,21 +3,22 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { AiOutlineEnter } from "react-icons/ai";
-import { FormEvent, useState } from "react";
+import { FormEvent, useRef, useState } from "react";
 import { sendPrompt } from "@/lib/axios";
-import { Message } from "@/components/ui/message";
+import { Message } from "@/components/ai/message";
+import { Loader2 } from "lucide-react";
 
 export default function Page() {
-  const [isAvailable, setIsAvailable] = useState(true);
-  const [prompt, setPrompt] = useState<string>("");
+  const [canPrompt, setCanPrompt] = useState(true);
   const [message, setMessage] = useState<string>("");
+  const promptRef = useRef<HTMLInputElement>(null);
 
   const getPromptResult = (e: FormEvent) => {
     e.preventDefault();
-    if (!isAvailable) return;
-    setIsAvailable(false);
+    if (!canPrompt || !promptRef.current) return;
+    setCanPrompt(false);
     setMessage("loading");
-    sendPrompt(prompt)
+    sendPrompt(promptRef.current.value)
       .then((res) => {
         setMessage(res);
       })
@@ -25,32 +26,35 @@ export default function Page() {
         setMessage("Error at getting response, try again.");
       })
       .finally(() => {
-        setPrompt("");
-        setIsAvailable(true);
+        promptRef.current!.value = "";
+        setCanPrompt(true);
       });
   };
 
   return (
     <main className="flex-1 flex flex-col justify-between mx-auto w-9/12 md:w-2/3 lg:w-9/12 xl:w-1/2">
-      <Message className="flex-auto content-center h-96" content={message} />
+      {message == "loading" ? (
+        <Loader2 className="m-auto size-12 animate-spin" />
+      ) : (
+        <Message className="flex-auto content-center h-96" content={message} />
+      )}
       <form
         onSubmit={getPromptResult}
         className="flex w-full mx-auto my-5 w-9/12 md:w-2/3 lg:w-9/12 xl:w-1/2"
       >
         <Input
           type="text"
+          ref={promptRef}
           className="me-2 h-12"
           placeholder="Send a prompt"
-          onChange={(e) => setPrompt(e.target.value)}
-          value={prompt}
-          disabled={!isAvailable}
+          disabled={!canPrompt}
           required
         />
         <Button
           type="submit"
           className="h-12"
           variant="outline"
-          disabled={!isAvailable}
+          disabled={!canPrompt}
           size="icon"
         >
           <AiOutlineEnter size={20} />
