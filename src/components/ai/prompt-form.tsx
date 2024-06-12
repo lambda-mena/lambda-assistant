a2eee6912bb46f8f9fcfc15847a3a46733ed49bd
VaatuDev <juanmena1605@gmail.com>
2024-06-12 17:39:54 -0500

    refactor: improve prompt-form component states

--- src/components/ai/prompt-form.tsx
+++ src/components/ai/prompt-form.tsx
@@ -9,31 +9,33 @@ import { Input } from "@/components/ui/input";
 import { Loader2 } from "lucide-react";
 
 export const PromptForm = () => {
-  const [message, setMessage] = useState("I'm happy to have you here");
-  const [canPrompt, setCanPrompt] = useState(true);
+  const [output, setOutput] = useState({ msg: "Hi!", isLoading: false });
   const inputRef = useRef<HTMLInputElement>(null);
 
-  function toggleForm(canPrompt: boolean, newMessage: string = "-") {
-    setCanPrompt(canPrompt);
-    setMessage(newMessage);
+  function toggleForm(isLoading: boolean, msg: string = "Hi!") {
+    setOutput({ isLoading, msg });
   }
 
   const onFormSubmit = async (e: FormEvent) => {
     e.preventDefault();
     if (!inputRef.current) return;
-    toggleForm(false);
+    toggleForm(true);
     const promptResult = await generateResponse(inputRef.current.value);
     inputRef.current.value = "";
-    toggleForm(true, promptResult);
+    toggleForm(false, promptResult);
   };
 
   return (
     <>
-      {message == "-" && <Loader2 className="m-auto size-12 animate-spin" />}
-      {message != "-" && <Message className="flex-auto animate__animated animate__fadeIn content-center h-96" content={message} /> }
+      {output.isLoading ? 
+      <Loader2 className="m-auto size-12 animate-spin" /> : 
+      <Message 
+        className="flex-auto animate__animated animate__fadeIn content-center h-96" 
+        content={output.msg} 
+      /> }
       <form onSubmit={onFormSubmit} className="flex animate__animated animate__fadeInUp mx-auto my-5 w-full md:w-2/3 lg:w-9/12 xl:w-1/2">
-        <Input ref={inputRef} className="me-2 h-12" placeholder="Send a prompt" disabled={!canPrompt} required />
-        <Button type="submit" className="h-12" variant="outline" disabled={!canPrompt} size="icon">
+        <Input ref={inputRef} className="me-2 h-12" placeholder="Send a prompt" disabled={output.isLoading} required />
+        <Button type="submit" className="h-12" variant="outline" disabled={output.isLoading} size="icon">
           <AiOutlineEnter size={20} />
         </Button>
       </form>
