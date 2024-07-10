5df19bfd55ab95d035bfefcfa6270663d2b5e0ae
lambda-mena <juanmena1605@gmail.com>
2024-07-10 12:06:31 -0500

    refactor: change from input to textarea

--- src/components/ai/prompt-form.tsx
+++ src/components/ai/prompt-form.tsx
@@ -1,15 +1,22 @@
 "use client";
 
 import { useChat } from "ai/react";
+import { KeyboardEvent, useRef } from "react";
 import { Button } from "@/components/ui/button";
 import { MessageArea } from "@/components/ui/message-area";
+import styles from "@/styles/scrollbar.module.css";
 import { AiOutlineEnter } from "react-icons/ai";
-import { Input } from "@/components/ui/input";
+import { Textarea } from "../ui/textarea";
 
 export const PromptForm = () => {
-  const { messages, input, handleInputChange, handleSubmit, isLoading } = useChat({
-    api: '/api/prompt'
-  });
+  const { messages, input, handleInputChange, handleSubmit, isLoading } = useChat({api: '/api/prompt'});
+  const ref = useRef<HTMLButtonElement>(null);
+
+  const clickSubmitButton = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key == 'Enter' && !e.shiftKey) {
+      ref.current!.click();
+    }
+  }
 
   return (
     <>
@@ -19,8 +26,15 @@ export const PromptForm = () => {
         isLoading={isLoading}
       /> : <div className="flex-auto h-96 m-auto text-lg content-center"> Try a prompt. </div>}
       <form onSubmit={handleSubmit} className="flex animate-in fade-in mx-auto my-5 w-full md:w-2/3 lg:w-9/12 xl:w-1/2">
-        <Input className="me-2 h-12" placeholder="Send a prompt" value={input} onChange={handleInputChange} disabled={isLoading} required />
-        <Button type="submit" className="h-12" variant="outline" disabled={isLoading} size="icon">
+        <Textarea
+          className={`${styles.scrollbar} me-2 min-h-1 h-12 max-h-96 text-lg resize-none`}
+          value={input}
+          onKeyDown={(e) => clickSubmitButton(e)}
+          onChange={handleInputChange}
+          disabled={isLoading}
+          required
+        />
+        <Button type="submit" className="h-12" variant="outline" ref={ref} disabled={isLoading} size="icon">
           <AiOutlineEnter size={20} />
         </Button>
       </form>
